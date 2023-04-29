14eb0d3cf81d9c33b336b892e6c8ad01dceb645b
Gantzhorn <andersgantzhorn@gmail.com>
2023-04-29 22:26:03 +0200

    exploring sim

--- explorative_environment/naiveHMMSim.R
+++ explorative_environment/naiveHMMSim.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(momentuHMM)
 theme_set(theme_bw())
-proj_palette <- pal <- c("#E69F00", "#56B4E9", "#009E73",
+proj_palette <- c("#E69F00", "#56B4E9", "#009E73",
                          "#F0E442", "#0072B2", "#D55E00",
                          "#CC79A7")
 
@@ -9,13 +9,13 @@ proj_palette <- pal <- c("#E69F00", "#56B4E9", "#009E73",
 # Define the parameters of the HMM
 N <- 3
 delta <- c(0.5, 0.25, 0.25)
-Gamma <- matrix(c(0.8, 0.1, 0.1,
-              0.1, 0.8, 0.1,
-              0.05, 0.05, 0.9),
+Gamma <- matrix(c(0.7, 0.15, 0.15,
+              0.15, 0.7, 0.15,
+              0.15, 0.15, 0.7),
             nrow = N, byrow = TRUE) # State transition matrix
 
 # Number of time-steps
-T <- 600
+T <- 3000
 
 # Simulate the hidden state sequence
 s <- rep(0, T)
@@ -25,11 +25,13 @@ s[2:T] <- sample(1:N, size = T-1, prob = Gamma[s[1:T-1],], replace=TRUE)
 obs_mat <- matrix(nrow = T, ncol = 2)
 
 # Parameters for gamma distributions of horizontal distances
-alpha <- c(10, 20, 75)
+alpha <- c(10, 20, 50)
 beta <- c(20, 5, 3.5)
 
+alpha/beta
+
 # Parameters for normal distributions for vertical distances
-mu <- c(-1, 0, 1)
+mu <- c(-3, 0, 3)
 sigma <- c(0.5, 0.25, 0.5)
 
 # X-coordinate
@@ -41,35 +43,27 @@ obs_mat[,2] <- rnorm(T, mean = mu[s], sd = sigma[s])
 
 # Return the simulated hidden state sequence and observation sequence
 simResult <- tibble(state = factor(s),
-                    x = obs_mat[,1], y = obs_mat[,2],
-                    ID = factor(rep(c(1,2,3), each = T/3))) %>%
-  tibble::rowid_to_column("time") %>%
-  mutate(time = case_when(
-    ID == "2" ~ time-T/3,
-    ID == "3" ~ time-2*(T/3),
-    TRUE ~ time
-  ))
+                    x = obs_mat[,1], y = obs_mat[,2]) %>%
+  tibble::rowid_to_column("time")
 # Nice plots
-# simResult %>% ggplot(aes(x = y, fill = state)) + geom_density()  + 
-#   scale_fill_manual(values = proj_palette)
-# 
-# simResult %>% ggplot(aes(x = log(x), y = y, color = state)) + geom_point()  + 
-#   scale_color_manual(values = proj_palette)
-# 
-# simResult %>% pivot_longer(cols = c("x", "y"),
-#                            names_to = "coord_lab", values_to = "coord_val") %>%
-#   mutate(coord_lab = factor(coord_lab)) %>%
-#   ggplot(aes(x = time, y = coord_val)) + geom_line() +
-#   facet_grid(coord_lab~ID, scales = "free") +
-#   geom_point(aes(x = time, y = coord_val, color = state)) + 
-#   scale_color_manual(values = proj_palette)
+simResult %>% ggplot(aes(x = y, fill = state)) + geom_density()  +
+  scale_fill_manual(values = proj_palette)
+
+simResult %>% ggplot(aes(x = log(x), y = y, color = state)) + geom_point()  +
+  scale_color_manual(values = proj_palette)
+
+simResult %>% pivot_longer(cols = c("x", "y"),
+                           names_to = "coord_lab", values_to = "coord_val") %>%
+  mutate(coord_lab = factor(coord_lab)) %>%
+  ggplot(aes(x = coord_val, color = state)) + geom_density() + 
+  facet_wrap(~coord_lab, scales = "free") + scale_color_manual(values = proj_palette)
 
 # Fit model
 # Init values for mean and variance of gamma and normal
-alpha0 <- c(1, 2, 2)
-beta0 <- c(2, 1, 0.25)
+alpha0 <- c(10, 20, 50)
+beta0 <- c(20, 5, 3.5)
 
-mu0 <- c(-1, 0, 1)
+mu0 <- c(-3, 0, 3)
 sigma0 <- c(0.5, 0.25, 0.5)
 
 # Fit the HMM model with momentuHMM package 1 dimension
