c790054a1fc11c4d111b94cdf33110882d6a0566
Gantzhorn <andersgantzhorn@gmail.com>
2023-05-01 16:30:58 +0200

    made function

--- explorative_environment/naiveHMMSim.R
+++ explorative_environment/naiveHMMSim.R
@@ -14,15 +14,7 @@ Gamma <- matrix(c(0.5, 0.25, 0.25,
             nrow = N, byrow = TRUE) # State transition matrix
 
 # Number of time-steps
-T <- 
-
-# Simulate the hidden state sequence
-s <- rep(0, T)
-s[1] <- sample(1:N, size = 1, prob = delta)
-s[2:T] <- sample(1:N, size = T-1, prob = Gamma[s[1:T-1],], replace=TRUE)
-# Simulate the observations
-obs_mat <- matrix(nrow = T, ncol = 2)
-
+T <- 500
 # Parameters for weibull distributions of horizontal distances
 k <- c(3, 20, 50)
 lambda <- c(5, 3, 3.5)
@@ -31,18 +23,33 @@ lambda <- c(5, 3, 3.5)
 mu <- c(-3, 0, 3)
 sigma <- c(0.5, 0.25, 0.5)
 
-# X-coordinate
-obs_mat[,1] <- rweibull(T, scale = lambda[s], shape = k[s])
-# Y-coordinate
-obs_mat[,2] <- rnorm(T, mean = mu[s], sd = sigma[s])
+simulate_HMM <- function(T, delta, Gamma, lambda, k, mu, sigma) {
+  
+  N <- length(delta)
+  # Simulate the hidden state sequence
+  s <- rep(0, T)
+  s[1] <- sample(1:N, size = 1, prob = delta)
+  s[2:T] <- sample(1:N, size = T-1, prob = Gamma[s[1:T-1],], replace=TRUE)
+  
+  # Simulate the observations
+  obs_mat <- matrix(nrow = T, ncol = 2)
+  
+  # X-coordinate
+  obs_mat[,1] <- rweibull(T, scale = lambda[s], shape = k[s])
+  # Y-coordinate
+  obs_mat[,2] <- rnorm(T, mean = mu[s], sd = sigma[s])
+  
+  # Return the simulated data as a list
+  return(list(s = s, obs_mat = obs_mat))
+}
 
-
+simRes <- simulate_HMM(T, delta, Gamma, lambda, k, mu, sigma)
 
 # Return the simulated hidden state sequence and observation sequence
-# simResult <- tibble(state = factor(s),
-#                     x = obs_mat[,1], y = obs_mat[,2]) %>%
+# simResult <- tibble(state = factor(simRes$s),
+#                     x = simRes$obs_mat[,1], y = simRes$obs_mat[,2]) %>%
 #   tibble::rowid_to_column("time")
-# # Nice plots
+# Nice plots
 # simResult %>% ggplot(aes(x = y, fill = state)) + geom_density()  +
 #   scale_fill_manual(values = proj_palette)
 # 
@@ -65,7 +72,7 @@ sigma0 <- sigma
 # Fit the HMM model with momentuHMM package 1 dimension
 
 # Prepare the data
-Prep_data1 <- momentuHMM::prepData(data = data.frame(vertical_steps = obs_mat[,2]),
+Prep_data1 <- momentuHMM::prepData(data = data.frame(vertical_steps = simRes$obs_mat[,2]),
                                   coordNames = NULL)
 
 modDim1 <- fitHMM(data = Prep_data1,
@@ -77,15 +84,15 @@ modDim1 <- fitHMM(data = Prep_data1,
 
 
 DecodedStates1 <- viterbi(m = modDim1) #Most likely state-sequence - compare to true state sequence.
-mean(DecodedStates1 == s) # Classification accuracy
+mean(DecodedStates1 == simRes$s) # Classification accuracy
 
-mean(DecodedStates1 != s)
+mean(DecodedStates1 != simRes$s)
 
 # Fit the HMM model with the momentuHMM package 2 dimensions
 
 # Prepare the data
-Prep_data2 <- momentuHMM::prepData(data = data.frame(horizontal_steps = obs_mat[,1],
-                                  vertical_steps = obs_mat[,2]),
+Prep_data2 <- momentuHMM::prepData(data = data.frame(horizontal_steps = simRes$obs_mat[,1],
+                                  vertical_steps = simRes$obs_mat[,2]),
                                   coordNames = NULL)
 
 modDim2 <- fitHMM(data = Prep_data2,
@@ -96,13 +103,11 @@ modDim2 <- fitHMM(data = Prep_data2,
                           vertical_steps = c(mu0, sigma0))
               )
 
-str(modDim2$mle$horizontal_steps)
-
 plot(modDim2)
 
 DecodedStates2 <- viterbi(m = modDim2) #Most likely state-sequence - compare to true state sequence.
 
 
-mean(DecodedStates2 == s) # Classification accuracy
+mean(DecodedStates2 == simRes$s) # Classification accuracy
 
-mean(DecodedStates2 != s)
+mean(DecodedStates2 != simRes$s)
