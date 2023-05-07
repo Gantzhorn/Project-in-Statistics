3630268b0235a084f2375db52b09cee84ec8a2d8
Gantzhorn <andersgantzhorn@gmail.com>
2023-05-07 10:22:14 +0200

    last nights work

--- explorative_environment/naiveHMMSim.R
+++ explorative_environment/naiveHMMSim.R
@@ -5,16 +5,31 @@ proj_palette <- c("#E69F00", "#56B4E9", "#009E73",
                          "#F0E442", "#0072B2", "#D55E00",
                          "#CC79A7")
 
+# Helper function
+split_and_stack <- function(x, ncol) {
+  # calculate number of rows needed
+  nrow <- length(x) / ncol
+
+  # add padding to x to make its length a multiple of ncol
+  x_pad <- c(x, rep(NA, nrow * ncol - length(x)))
+
+  # create matrix with desired number of columns and rows
+  mat <- matrix(x_pad, nrow = nrow, ncol = ncol, byrow = TRUE)
+
+  return(mat)
+}
+
+
 # Define the parameters of the HMM
 N <- 3
 delta <- c(0.33, 0.33, 0.33)
-Gamma <- matrix(c(0.5, 0.25, 0.25,
-                  0.25, 0.5, 0.25,
-                  0.25, 0.25, 0.5),
+Gamma <- matrix(c(0.8, 0.1, 0.1,
+                  0.1, 0.8, 0.1,
+                  0.1, 0.1, 0.8),
             nrow = N, byrow = TRUE) # State transition matrix
 
 # Number of time-steps
-T <- 2000
+T <- 5000
 # Parameters for weibull distributions of horizontal distances
 k <- c(3, 10, 12)
 lambda <- c(2, 4, 6)
@@ -23,44 +38,45 @@ lambda <- c(2, 4, 6)
 mu <- c(-1, 0, 1)
 sigma <- c(1, 0.5, 1)
 
-simulate_HMM <- function(T, delta, Gamma, lambda, k, mu, sigma) {
-  
+simulate_HMM <- function(T, delta, Gamma, dists, params) {
+
   N <- length(delta)
+
   # Simulate the hidden state sequence
   s <- rep(0, T)
   s[1] <- sample(1:N, size = 1, prob = delta)
   s[2:T] <- sample(1:N, size = T-1, prob = Gamma[s[1:T-1],], replace=TRUE)
-  
+
   # Simulate the observations
   obs_mat <- matrix(nrow = T, ncol = 2)
-  
+
   # X-coordinate
-  obs_mat[,1] <- rweibull(T, scale = lambda[s], shape = k[s])
+  dist1 <- match.fun(dists[1])
+  params_dist1 <- split_and_stack(params[[1]], ncol = N)
+  if (nrow(params_dist1) == 1){
+    obs_mat[, 1] <- dist1(n = T, params_dist1[1, s])
+  }
+  else {
+    obs_mat[, 1] <- dist1(n = T, params_dist1[1, s], params_dist1[2, s])
+  }
+
+
   # Y-coordinate
-  obs_mat[,2] <- rnorm(T, mean = mu[s], sd = sigma[s])
-  
+  dist2 <- match.fun(dists[2])
+  params_dist2 <- split_and_stack(params[[2]], ncol = N)
+  if (nrow(params_dist2) == 1){
+    obs_mat[,2] <- dist2(n = T, params_dist2[1, s])
+  }
+  else {
+    obs_mat[,2] <- dist2(n = T, params_dist2[1, s], params_dist2[2, s])
+  }
+
+
   # Return the simulated data as a list
   return(list(s = s, obs_mat = obs_mat))
 }
 
-simRes <- simulate_HMM(T, delta, Gamma, lambda, k, mu, sigma)
-
-# Return the simulated hidden state sequence and observation sequence
-simResult <- tibble(state = factor(simRes$s),
-                    x = simRes$obs_mat[,1], y = simRes$obs_mat[,2]) %>%
-  tibble::rowid_to_column("time")
-# Nice plots
-simResult %>% ggplot(aes(x = y, fill = state)) + geom_density()  +
-  scale_fill_manual(values = proj_palette)
-
-simResult %>% ggplot(aes(x = x, y = y, color = state)) + geom_point()  +
-  scale_color_manual(values = proj_palette)
-
-simResult %>% pivot_longer(cols = c("x", "y"),
-                           names_to = "coord_lab", values_to = "coord_val") %>%
-  mutate(coord_lab = factor(coord_lab)) %>%
-  ggplot(aes(x = coord_val, fill = state)) + geom_density() +
-  facet_wrap(~coord_lab, scales = "free") + scale_fill_manual(values = proj_palette)
+simRes <- simulate_HMM(T, delta, Gamma, dists = c("rweibull", "rnorm"), list(c(k, lambda), c(mu, sigma)))
 
 # Fit model
 # Init values for mean and variance of gamma and normal
@@ -112,14 +128,35 @@ mean(DecodedStates2 == simRes$s) # Classification accuracy
 
 mean(DecodedStates2 != simRes$s)
 
+# Sim from model:
+M <- 30
+meanClassification <- numeric(M)
+
+for (i in 1:M){
+newSim <- simData(model = modDim2, nbStates = 3, obsPerAnimal = 5000)
+
+modSimFit <- fitHMM(data = newSim,
+                  nbStates = N,
+                  dist = list(horizontal_steps = "weibull",
+                              vertical_steps = "norm"),
+                  Par0 = list(horizontal_steps = c(k0, lambda0),
+                              vertical_steps = c(mu0, sigma0))
+)
+
+DecodedStatesSim <- viterbi(m = modSimFit)
+meanClassification[i] <- mean(DecodedStatesSim == simRes$s)
+}
+
+
+
 ### Accessing elements from the momentuHMM fit object
 modDim2$data # data used for fitting
 modDim2$mle # Estimate of parameters of observed chain, initial distribution
-# & probability transition matrix 
+# & probability transition matrix
 
 modDim2$CIreal # Estimates and confidence intervals of the above
 
-modDim2$mod # mod estimates, gradient, hessian, numIter, min of neg loglik 
+modDim2$mod # mod estimates, gradient, hessian, numIter, min of neg loglik
 # time to conv. etc.
 
 # play with the objects
@@ -132,14 +169,14 @@ colnames(verticalStepsmat) <- c("estState1", "estState2", "estState3",
                                 "lowerState1", "lowerState2", "lowerState3",
                                 "upperState1", "upperState2", "upperState3")
 
-verticalSteps <- as_tibble(verticalStepsmat, rownames = "Parameter") %>% 
-  pivot_longer(cols = -Parameter, names_to = "Info",values_to = "Value") %>% 
+verticalSteps <- as_tibble(verticalStepsmat, rownames = "Parameter") %>%
+  pivot_longer(cols = -Parameter, names_to = "Info",values_to = "Value") %>%
   mutate(State = factor(str_sub(Info, start = -1L, end = -1L)), Parameter = factor(Parameter),
          Type = factor(case_when(str_detect(Info,"estState") ~ "Estimate",
                           str_detect(Info,"lowerState") ~ "Lower",
                           str_detect(Info,"upperState")  ~ "Upper"
-                          ))) %>% select(Parameter, Type, State, Value) %>% 
-  pivot_wider(names_from = "Type", values_from = "Value") %>% 
+                          ))) %>% select(Parameter, Type, State, Value) %>%
+  pivot_wider(names_from = "Type", values_from = "Value") %>%
   mutate(trueParam = c(mu, sigma))
 
 verticalSteps %>% ggplot(aes(x = State, y = Estimate)) +
